de180494e4b174e091b5b0ceda7b6c6e613db808
itjope <itjope@users.noreply.github.com>
2021-04-02 00:37:11 +0200

    Game settings UI

---
+++ Lobby.gd
@@ -0,0 +1,51 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var gameOptions = {
+	race = 5,
+	practice = 0,
+	qualifying = 0,
+	tracks = 1
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.get_node("LapsSlider").connect("value_changed", self, "_on_laps_changed")
+	self.get_node("QualifySlider").connect("value_changed", self, "_on_qualify_changed")
+	self.get_node("PracticeSlider").connect("value_changed", self, "_on_practice_changed")
+	self.get_node("TracksSlider").connect("value_changed", self, "_on_tracks_changed")
+	
+func _to_minutes_str(minutes):
+	var value = str(minutes) + " minutes"
+	if (minutes < 1):
+		value = "Off"
+	if (minutes == 1):
+		value = str(minutes) + " minute"
+	return value
+
+func _to_laps_str(laps):
+	var value = str(laps) + " laps"
+	
+	if (laps == 1):
+		value = str(laps) + " lap"
+	return value
+
+func _on_laps_changed(laps):
+	gameOptions.race = laps
+	self.get_node("LapsValueLabel").text = _to_laps_str(laps)
+
+
+func _on_qualify_changed(laps):
+	gameOptions.qualify = laps
+	self.get_node("QualifyValueLabel").text = _to_minutes_str(laps)
+
+func _on_practice_changed(laps):
+	gameOptions.practice = laps
+	self.get_node("PracticeValueLabel").text = _to_minutes_str(laps)
+
+func _on_tracks_changed(tracks):
+	gameOptions.tracks = tracks
+	self.get_node("TracksValueLabel").text = str(tracks)
--- ServerOrJoin.gd
+++ ServerOrJoin.gd
@@ -2,6 +2,9 @@ extends Control
 
 func _ready():
 	randomize()
-	var randomNumber = str(randi() % 51 + 10)
-	self.get_node("PlayerNameInput").text = "SquirmyBroom" + randomNumber
+	var number = 35
+	while(number == 35): 
+		number = randi() % 51 + 10
+
+	self.get_node("PlayerNameInput").text = "squirmybroom" + str(number)
 
