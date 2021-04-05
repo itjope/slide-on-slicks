5e9e2eb90f84a55dc908eadceebb1bb6705e33a3
itjope <itjope@users.noreply.github.com>
2021-04-05 21:26:33 +0200

    Display number of laps for each player

--- World.gd
+++ World.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
 func _ready():
-	print("ready")
+	$Track.connect("finished_lap", self, "_on_finished_lap")
+	$Server.connect("start_game", self, "_on_start_game")
+
+func _on_finished_lap(body):
+	$Race/RaceInfo.on_finished_lap(body)
 	
+func _on_start_game(my_info, player_info):
+	$Race/RaceInfo.on_start_game(my_info, player_info)
